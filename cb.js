1f91575c0eabc409187fda33ba5d4c8ce9ac6fea
Alex Kilbo <akilbo@gmail.com>
2012-03-29 15:01:25 -0700

    added lightbox, need to generalize it to create multiple versions

--- cb.js
+++ cb.js
@@ -1,29 +1,36 @@
-    function gradient(id, level){
-    var box = document.getElementById(id);
-      box.style.opacity = level;
-      box.style.MozOpacity = level;
-      box.style.KhtmlOpacity = level;
-      box.style.filter = "alpha(opacity=" + level * 100 + ")";
-      box.style.display="block";
-      return;
-    }   
 
 
-    function fadein(id) {
-      var level = 0;
-      while(level <= 1){
-        setTimeout( "gradient('" + id + "'," + level + ")", (level* 1000) + 10);
-        level += 0.01;
-      }
-    }
+function gradient(id, level)
+{
+  var box = document.getElementById(id);
+  box.style.opacity = level;
+  box.style.MozOpacity = level;
+  box.style.KhtmlOpacity = level;
+  box.style.filter = "alpha(opacity=" + level * 100 + ")";
+  box.style.display="block";
+  return;
+}
+
+
+function fadein(id) 
+{
+  var level = 0;
+  while(level <= 1)
+  {
+    setTimeout( "gradient('" + id + "'," + level + ")", (level* 1000) + 10);
+    level += 0.01;
+  }
+}
 
 
 // Open the lightbox
 
 
-  function openbox(formtitle, fadin){
+function openbox(formtitle, fadin)
+{
   var box = document.getElementById('box'); 
-  document.getElementById('filter').style.display='block';
+  document.getElementById('shadowing').style.display='block';
+
   var btitle = document.getElementById('boxtitle');
   btitle.innerHTML = formtitle;
   
@@ -36,13 +43,15 @@
   {   
     box.style.display='block';
   }   
-} 
+}
 
 
 // Close the lightbox
 
-    function closebox(){
-        document.getElementById('box').style.display='none';
-        document.getElementById('shadowing').style.display='none';
-    }
+function closebox()
+{
+   document.getElementById('box').style.display='none';
+   document.getElementById('shadowing').style.display='none';
+}
+
 
